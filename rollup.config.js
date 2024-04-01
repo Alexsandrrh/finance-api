dd497a3eb368f8dc94c942716b64e1384fa3931d
Sadov Aleksandr Denisovich <sascha.sadov@yandex.ru>
2024-04-01 18:39:35 +0300

    feature: Доработана сборка

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,18 @@
+import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
+
+import packageJson from './package.json' with { type: 'json' };
 
 export default {
   input: './src/index.ts',
   output: [
-    { dir: 'dist/cjs', format: 'cjs', sourcemap: true },
-    { dir: 'dist/esm', format: 'esm', sourcemap: true },
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+      plugins: [commonjs()],
+    },
+    { file: packageJson.module, format: 'esm', sourcemap: true },
   ],
   plugins: [typescript({ useTsconfigDeclarationDir: true })],
 };
