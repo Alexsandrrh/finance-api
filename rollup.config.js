b6430c42be37c56c560d8b17c8d821f074f71577
Sadov Aleksandr Denisovich <sascha.sadov@yandex.ru>
2024-04-01 14:55:42 +0300

    feature: Доработан конфиг

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,8 @@ import typescript from 'rollup-plugin-typescript2';
 export default {
   input: './src/index.ts',
   output: [
-    { dir: 'dist/cjs', format: 'cjs' },
-    { dir: 'dist/esm', format: 'esm' },
+    { dir: 'dist/cjs', format: 'cjs', sourcemap: true },
+    { dir: 'dist/esm', format: 'esm', sourcemap: true },
   ],
   plugins: [typescript({ useTsconfigDeclarationDir: true })],
 };
