a3d726f358aad02eda78a7537f24d2ddf268c18f
Sadov Aleksandr Denisovich <sascha.sadov@yandex.ru>
2024-04-01 19:39:51 +0300

    feature: Добавлены новые поля для GetOrdersAlgoPendingDataItem

--- src/okx/TradingBot/TradingBotGrid/types/GetOrdersAlgoPendingDataItem.ts
+++ src/okx/TradingBot/TradingBotGrid/types/GetOrdersAlgoPendingDataItem.ts
@@ -1,8 +1,31 @@
+import { AlgoOrdType } from './AlgoOrdType';
+
 export interface GetOrdersAlgoPendingDataItem {
   algoId: string;
-  instId: string;
+  algoClOrdId: string;
   instType: string;
+  instId: string;
+  cTime: string;
+  uTime: string;
+  algoOrdType: AlgoOrdType;
   state: 'starting' | 'running' | 'stopping' | 'no_close_position';
+  rebateTrans: [];
+  rebate: string;
+  rebateCcy: string;
+  triggerParams: [];
+  triggerAction: 'start' | 'stop';
+  triggerStrategy: 'instant' | 'price' | 'rsi';
+  delaySeconds: string;
+  triggerTime: string;
+  triggerType: 'manual' | 'auto';
+  timeframe: string;
+  thold: string;
+  triggerCond: 'cross_up' | 'cross_down' | 'above' | 'below' | 'cross';
+  timePeriod: string;
+  triggerPx: string;
+  stopType: string;
+  maxPx: string;
+  minPx: string;
   gridProfit: string;
   floatProfit: string;
   investment: string;
